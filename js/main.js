0996805bc90e4742e9670801b55aa82e4314f7b8
エンジニアkuro <ryoichi.kuroda@outlook.jp>
2024-12-27 15:06:36 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,111 @@
+$(function () {
+  /*=================================================
+  スマホメニュー
+  ===================================================*/
+  // ハンバーガーメニューのクリックイベント
+  $(".toggle_btn").on("click", function () {
+
+    ($("#header").toggleClass("open"));
+  });
+
+  $("#mask").on("click", function () {
+
+    $("#header").toggleClass("open");
+
+  });
+
+  $("#navi a").on("click", function () {
+
+    $("#header").toggleClass("open");
+  });
+
+
+
+  /*=================================================
+  スムーススクロール
+  ===================================================*/
+  // ページ内リンクのイベント
+  $('a[href^="#"]').click(function () {
+    let href = $(this).attr("href");
+
+    let target = $(href == "#" || href == "" ? "html" : href);
+
+    let position = target.offset().top;
+
+    $("html, body").animate({ scrollTop: position }, 600, "swing");
+    return false;
+  });
+
+
+  /*=================================================
+  スクロール時の画像フェード表示
+  ===================================================*/
+  // スクロール時のイベント
+  $(window).scroll(function () {
+    // 画面がスクロールされた時に実行する
+
+    $(".fadein").each(function () {
+      // fadeinクラスに対して順に処理を行う
+      // .each()：個別に処理を行うためのメソッド。繰り返し処理を行いながら各要素に対して操作を実行することができる。
+
+
+      // スクロールした距離
+      let scroll = $(window).scrollTop();
+      // 現在のスクロール位置を取得する。
+      // scrollTop()：要素のスクロール位置を取得
+
+      // fadeinクラスの要素までの距離
+      let target = $(this).offset().top;
+
+      // 画面の高さ
+      let windowHeight = $(window).height();
+
+      // fadeinクラスの要素が画面内にきたタイミングで要素を表示
+      if (scroll > target - windowHeight + 200) {
+
+        // 条件が満たされた場合、要素の不透明度（opacity）を1に設定し、Y軸方向に移動（translateY）させます。
+        $(this).css("opacity", "1");
+        $(this).css("transform", "translateY(0)");
+      }
+    });
+
+  });
+});
+
+
+/*=================================================
+ローディングアニメーション
+===================================================*/
+
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+  easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+  duration: 3500,//時間指定(1000＝1秒)
+  strokeWidth: 0.2,//進捗ゲージの太さ
+  color: '#555',//進捗ゲージのカラー
+  trailWidth: 0.2,//ゲージベースの線の太さ
+  trailColor: '#bbb',//ゲージベースの線のカラー
+  text: {//テキストの形状を直接指定				
+    style: {//天地中央に配置
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      padding: '0',
+      margin: '-30px 0 0 0',//バーより上に配置
+      transform: 'translate(-50%,-50%)',
+      'font-size': '1rem',
+      color: '#fff',
+    },
+    autoStyleContainer: false //自動付与のスタイルを切る
+  },
+  step: function (state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+  }
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+  $("#splash_text").fadeOut(10);//フェイドアウトでローディングテキストを削除
+  $(".loader_cover-up").addClass("coveranime");//カバーが上に上がるクラス追加
+  $(".loader_cover-down").addClass("coveranime");//カバーが下に下がるクラス追加
+  $("#splash").fadeOut();//#splashエリアをフェードアウト
+});
